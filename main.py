aac5be54069c164b5f259fd6f6d4917b3d22bcf4
PedroRossa98 <pedro.rossa@tecnico.ulisboa.pt>
2023-02-24 19:39:34 +0000

    update

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def Mauser_pressure(old, new):
 
 if __name__ == "__main__":
     status = GPIO.Int_GPIO()
-    new_p = VSR53USB({"COM":'/dev/ttyUSB1',"timeout":1})
+    new_p = VSR53USB.VSR53USB({"COM":'/dev/ttyUSB1',"timeout":1})
     new_p.Adj_Gas_Correctoion_Factor(1)
     old = PPT200('/dev/ttyUSB0')
     data_thread = threading.Thread(target=Mauser_pressure,args=(old,new_p,),daemon=True)
