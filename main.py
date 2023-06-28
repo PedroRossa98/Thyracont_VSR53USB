03ae3284a5748d9fbe939c90dcbb17b129e4f229
PedroRossa98 <PedroRossa98@tecnico.ulisboa.pt>
2023-06-28 14:11:58 +0100

    python code to compar PPT200 with the VSR53USB

--- main.py
+++ main.py
@@ -20,60 +20,82 @@ ON = GPIO.ON
 
 presure_new = None
 presure_old = None
+new_pirani= None
+new_piezo = None
 Stp = True
 print_out = False
+time_s = 0
 
 f = None
 
 def Mauser_pressure(old, new):
     global presure_old
     global presure_new
+    global new_piezo
+    global new_pirani
     while True:
         presure_old = "{:.3f}".format(PPT200.get_pressure(old))
         presure_new = new.Pressure()
+        new_pirani = new.Pressure_Pirani()
+        new_piezo = new.Pressure_Piezo()
         if print_out:
             print(float(presure_old)*100)
         time.sleep(0.001)
     return
 
-def Save_data(f):
+def Save_data():
     global presure_old
     global presure_new
+    global new_piezo
+    global new_pirani
     global Stp
+    global f
+    global time_s
+    printing = 0
     while Stp:
-        f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"\t"+str(float(presure_old)*100)+"\t"+str(float(presure_new.decode('ascii'))*100)+"\n")
-        time.sleep(2)
+        try:
+            print_piezo_m = str(float(new_piezo.decode('ascii'))*100)
+        except:
+            print_piezo_m = str(new_piezo.decode('ascii'))
+        if time_s != 0:
+             printing = 1
+        f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"\t"+str(time_s)+"\t"+str(float(presure_old)*100)+"\t"+str(float(presure_new.decode('ascii'))*100)+"\t"+str(float(new_pirani.decode('ascii'))*100)+"\t"+print_piezo_m+"\n")
+        time.sleep(0.1)
+        if time_s != 0 and printing ==1:
+             time_s = 0
     return
 
 def init_save_file():
+    global f
     f = open("Comperation_gauge.txt", "w")
-    f.write("Time\tPressure Old [Pa]\tPressure New [Pa]\n")
+    f.write("Time\tTime injected [ms]\tPressure Old [Pa]\tPressure New [Pa]\tNew Pressure Pirani Mode [Pa]\tNew Pressure Piezo Mode [Pa]\n")
     f.flush()
     return f
 
 
 if __name__ == "__main__":
     status = GPIO.Int_GPIO()
-    new_p = VSR53USB.VSR53USB({"COM":'/dev/ttyUSB1',"timeout":1})
+    new_p = VSR53USB.VSR53USB({"COM":'/dev/tty_pressure',"timeout":1})
     new_p.Adj_Gas_Correctoion_Factor(1)
     old = PPT200.int_com_PPT200('/dev/ttyUSB0')
     data_thread = threading.Thread(target=Mauser_pressure,args=(old,new_p,),daemon=True)
     data_thread.start()
     f = init_save_file()
-    data_collection = threading.Thread(target=Save_data,args=(f,),daemon=True)
-    
+    data_collection = threading.Thread(target=Save_data,daemon=True)
+    time.sleep(1)
     while True:
-        print(float(presure_old)*100)
+        print(str(float(presure_old)*100)+"\t"+str(float(presure_new.decode('ascii'))*100))
         input_msg = input()
         print(input_msg)
         if input_msg == 'vac':
             GPIO.Vacum_Pump_stat(ON)
+            GPIO.Tomanda_energia_stat(ON)
             time.sleep(2)
             GPIO.Valve_cut_off_stat(ON)
         elif input_msg == 'vacf':
             GPIO.Vacum_Pump_stat(OFF)
             time.sleep(2)
-            GPIO.Valve_cut_off_stat(OFF)
+            #GPIO.Valve_cut_off_stat(OFF)
         elif input_msg == 'he':
             time_s = input()
             GPIO.Inject_Gas(1, int(time_s))
@@ -95,5 +117,9 @@ if __name__ == "__main__":
             print("Closing file!\n\r")
             f.flush()
             f.close()
+        elif input_msg == 'exit':
+            GPIO.Tomanda_energia_stat(ON)
+            time.sleep(1)
+            GPIO.Valve_cut_off_stat(ON)
         else:
             pass
